17a514513663e83318846a5a146b6b2426b18c7d
Ruttik Hiwase <89509037+RuttikHiwase@users.noreply.github.com>
2025-04-11 12:09:46 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,20 @@
+class first{
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++)
+        {
+         for(int l=1;l<=5;l++)
+
+            {
+                for (int k = 1; k <= 5; k++) {
+                    for (int j = 5; j >=1; j--) {
+                        System.out.print("*");
+                    }
+                    System.out.print("     ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+
+        }
+    }
+}
